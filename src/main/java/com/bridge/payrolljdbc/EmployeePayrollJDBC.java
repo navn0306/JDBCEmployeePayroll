512651bc9351b1ce14d1ee7e5153b3888032dc4c
Navneet <navn0306@gmail.com>
2022-01-10 02:08:04 +0530

    ADDED: Welcome message for program

---
+++ src/main/java/com/bridge/payrolljdbc/EmployeePayrollJDBC.java
@@ -0,0 +1,11 @@
+package com.bridge.payrolljdbc;
+
+public class EmployeePayrollJDBC {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to JDBC Program");
+    }
+
+}
+
+
