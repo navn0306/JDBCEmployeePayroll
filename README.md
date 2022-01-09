aa4c464f71d16979d903568ce00d69d588c384ac
Navneet <navn0306@gmail.com>
2022-01-10 02:12:59 +0530

    ADDED: Ability to create a payroll service database and have java program connect to database.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JDBCEmployeePayroll
 
 Welcome to JDBC Employee Payroll
+
+UseCase1 -
+Ability to create a payroll service database and have java program connect to database.
--- src/main/java/com/bridge/payrolljdbc/EmployeePayrollJDBC.java
+++ src/main/java/com/bridge/payrolljdbc/EmployeePayrollJDBC.java
@@ -1,11 +1,39 @@
 package com.bridge.payrolljdbc;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class EmployeePayrollJDBC {
     public static void main(String[] args) {
 
-        System.out.println("\n Welcome to JDBC Program");
+        // Step 1- Loading Driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not loaded");
+        }
+
+        listDrivers();
+        //Step 2- Making Connection
+        String JDBCURL = "jdbc:mysql://localhost:3306/payroll_service";
+        try {
+            DriverManager.getConnection(JDBCURL, "root", "mysql@123");
+            System.out.println("Connection Established");
+        } catch (SQLException e) {
+            System.out.println("Connection not established");
+        }
     }
 
+    // To print Drivers
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driver = (Driver) driverList.nextElement();
+            System.out.println("Driver Names : " + driver.getClass().getName());
+        }
+    }
 }
-
 
